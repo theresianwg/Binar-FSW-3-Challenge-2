6686892ee90ca6da557d45d2420f684bd7dd8d7e
theresianwg <theresianawangsih@gmail.com>
2023-03-17 00:26:12 +0700

    test

---
+++ scripts/sortCarByYearAscendingly.js
@@ -0,0 +1,23 @@
+function sortCarByYearAscendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+
+  // Tulis code-mu disini
+
+  for ( let i=0; i < result.length; i++){
+    for ( let j=0; j < result.length-1; j++){
+      if (result[j].year > result[j+1].year ){
+        let temp = result[j]
+        result[j] = result[j+1]
+        result[j+1] = temp
+        // console.log(temp);
+      }
+    }
+  }
+  // Rubah code ini dengan array hasil sorting secara ascending
+  return result;
+}
